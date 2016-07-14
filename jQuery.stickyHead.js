748f6d62fc661c73554f67e84a0dbf9099ede6f6
exabyssus <agw@agw.lv>
2016-07-14 13:47:50 +0300

    Update to latest version

--- jQuery.stickyHead.js
+++ jQuery.stickyHead.js
@@ -1,66 +1,69 @@
 // stickyHead v0.1 - https://github.com/exabyssus/stickyHead - MIT Licensed
 (function( $ ){
 
-  $.fn.stickyHead = function(op){
+    $.fn.stickyHead = function(op){
 
-      if( ! this.length)
-          return false;
+        if( ! this.length)
+            return false;
 
-      var options = $.extend({
-          topStickOffset : 100, // How far header will stick to the top before scrolling away. (default: 100) (px),
-          addPadding : true // If true, will add padding-top to body. (default: true)
-      }, op);
+        var options = $.extend({
+            topStickOffset : 100, // How far header will stick to the top before scrolling away. (default: 100) (px),
+            addPadding : true // If true, will add padding-top to body. (default: true)
+        }, op);
 
-      var el = $(this).eq(0);
+        var el = $(this).eq(0);
 
-      var elHeight = el.outerHeight();
-      var lstPos = 0;
+        var elHeight = el.outerHeight();
 
-      var fOut = true;
+        var lstPos = 0;
+        var dChange = 0;
 
-      el.css('position', 'absolute')
-          .css('top', $(window).scrollTop())
-          .css('left', 0)
-          .css('width', '100%');
+        el.css('position', 'fixed')
+            .css('top', $(window).scrollTop())
+            .css('left', 0)
+            .css('width', '100%');
 
-      if(options.addPadding)
-          $('body').css('padding-top', elHeight);
+        if(options.addPadding)
+            $('body').css('padding-top', elHeight);
 
 
-      $(window).on('scroll', function(e){
+        $(window).on('scroll', function(){
 
-          var curPos = $(this).scrollTop();
+            var curPos = $(this).scrollTop();
 
-          var curElPos = el.offset().top;
+            var curElPos = el.offset().top;
 
-          if(curPos < lstPos || curPos < options.topStickOffset)
-          {
-              if(curPos > curElPos + elHeight && fOut)
-              {
-                  fOut = false;
 
-                  var offset = curPos > elHeight * 2 ? elHeight * 2 : elHeight;
+            if(curPos > options.topStickOffset)
+            {
+                // scroll up
+                if(curPos < lstPos) {
 
-                  if(curPos < elHeight)
-                  {
-                      offset = 0;
-                  }
+                    var offset = curPos > elHeight * 2 ? elHeight * 2 : elHeight;
 
-                  el.css('top', curPos - offset);
-              }
+                    if (dChange == 1)
+                    {
+                        el.css('position', 'absolute').css('top', curPos - offset);
+                    }
+                    else if (curPos <= curElPos) {
+                        el.css('position', 'fixed').css('top', 0);
+                    }
 
-              if(curPos < curElPos || curPos < options.topStickOffset)
-              {
-                  el.css('position', 'absolute').css('top', curPos);
-              }
-          }
-          else
-          {
-              fOut = true;
-          }
+                    dChange = -1;
+                }
+                // scroll down
+                else {
 
-          lstPos = curPos;
-      });
-  };
+                    if(dChange == -1) {
+                        el.css('position', 'absolute').css('top', curPos);
+                    }
+
+                    dChange = 1;
+                }
+            }
+
+            lstPos = curPos;
+        });
+    };
 
 })( jQuery );
